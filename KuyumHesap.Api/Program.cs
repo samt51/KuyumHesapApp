bc1afe584a2a8232e2c4c4aecd0074df53830235
kuyumhesap.admin <kuyumhesap.admin@WIN-53Q4H0MHTJA>
2026-01-18 04:26:29 +0300

    production ortam eklendi

--- KuyumHesap.Api/Program.cs
+++ KuyumHesap.Api/Program.cs
@@ -89,11 +89,10 @@ builder.Services.AddSwaggerGen(c =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+ 
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+ 
 
 app.UseHttpsRedirection();
 app.ConfigureExceptionHandlingMiddleware();
